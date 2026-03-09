a5508ee88db276bec5904ff0d2afbd8f341dd401
matorrec1980 <matorrec@gmail.com>
2026-03-09 10:57:58 -0500

    Update supabase.js

--- core/supabase.js
+++ core/supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://rnoprngmmqvrzfygyppg.supabase.co";
 
+const SUPABASE_ANON_KEY =
+"sb_publishable_zLWA_SDD0Y6Jt3ADtdmwZg_M95oKx8W";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
